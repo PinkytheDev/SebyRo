fe6ef8e953cab5d90a499c34e37aba7a0b3f3609
PinkytheDev <47577706+PinkytheDev@users.noreply.github.com>
2019-02-16 19:41:44 +0200

    Update mcpecombot.py

--- mcpecombot.py
+++ mcpecombot.py
@@ -14,6 +14,7 @@ async def on_message(message):
     author = message.author
     content = message.content
     print('{}: {}'.format(author, content))
+    await client.process_commands(message)
 
 @client.event
 async def on_message_delete(message):
@@ -21,19 +22,18 @@ async def on_message_delete(message):
     content = message.content
     channel = message.channel
     await client.send_message(channel, '{}: {}'.format(author, content))
+    await client.process_commands(message)
 
-@client.event
-async def on_message(message):
-    channel = message.channel
-    if message.content.startswith('?ping'):
-        await client.send_message(channel, 'Pong!')
+@client.command()
+async def ping ():
+    await client.say('Pong!')
 
-    if message.content.startswith('?echo'):
-        msg = message.content.split()
-        output = ''
-        for word in msg[1:]:
-            output += word
-            output += ' '
-        await client.send_message(channel, output)
+@client.command():
+async def echo(*args):
+    output = ''
+    for word in args:
+        output += word
+        output += ' '
+    await client.say(output)
 
 client.run(TOKEN)
